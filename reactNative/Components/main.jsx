617d4c830e81e815fb227e8ea68343539e3fe469
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-04-10 10:10:37 -0400

    Carpeta para componentes de React-Native

---
+++ reactNative/Components/main.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+const MainPage = () => {
+    return(
+        <View>
+            <Text>HOLA XD</Text>
+        </View>
+    )
+}
+
+export default MainPage
