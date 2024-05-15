e6493393cfa9a79d5d5c9b3521eea652be876298
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2024-05-15 15:18:52 +0000

    implementación de fondo segun el mockup

--- reactNative/Components/main.jsx
+++ reactNative/Components/main.jsx
@@ -1,12 +1,37 @@
-import { View, Text } from "react-native";
-
-
+import { View, Text,ImageBackground, StyleSheet} from "react-native";
+import fondo from '../images/fondo.png';
 const MainPage = () => {
     return(
-        <View>
-            <Text>HOLA XD</Text>
+        <View style={styles.container}>
+            <ImageBackground 
+              source={fondo} 
+              resizeMode="cover" 
+              style={styles.image}
+            >
+              <Text>Hola</Text>
+            </ImageBackground>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'center',
+      height:'100vh',
+      width:'100vw'
+    },
+    text: {
+      color: 'white',
+      fontSize: 42,
+      lineHeight: 84,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      backgroundColor: '#000000c0',
+    },
+  });
 
 export default MainPage
