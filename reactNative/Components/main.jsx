ad8bb477f2c8fbff024d760a430e6c7c224406c0
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-05-29 18:36:12 -0400

    Se añadió front del login

--- reactNative/Components/main.jsx
+++ reactNative/Components/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Box, FormControl, Image, Input, Stack, Text} from "native-base";
-import { FaSmile } from "react-icons/fa";
+import { Box, Button, Column, FormControl, Image, Input, Row, Stack, Text} from "native-base";
+import { FaArrowRight, FaLock, FaUser } from "react-icons/fa";
 
 //import  from "../assets/fonts/RutaCL_Regular.ttf"
-//import logo from "../images/"
+import logo from "../images/Logo.png"
+//import { border } from "native-base/lib/typescript/theme/styled-system";
 
 const MainPage = () => {
     return(
@@ -19,7 +20,7 @@ const MainPage = () => {
           }
         }}
         alignItems="center" justifyContent="center" padding={"6"}>
-        {/*<Image source={logo} alt="logo" size={"x1"}/>*/}
+        <Image style={{width:"39%"}} source={logo} alt="logo" size={"xl"}/>
         <Text 
           fontWeight={"bold"} 
           fontSize={"6xl"} 
@@ -28,13 +29,33 @@ const MainPage = () => {
         >BacheApp</Text>
         <FormControl>
           <Stack space={5}>
+            
             <Stack>
-              <FormControl.Label>Username</FormControl.Label>
-              <Input variant="underlined" p={2} placeholder="Username" />
+              <Box borderRadius={"md"} display={"flex"} alignSelf={"center"} shadow={2} bg={"white"} width={"75%"}>
+                <Row display={"flex"}>
+                  <Column w={"8%"} p={2}><FaUser/></Column>
+                  <Column w={"92%"}><Input variant="unstyled" w="100%" borderStyle={"none"} type="text" fontFamily={'fuenteRegular'} p={2} placeholder="Username" />  </Column>
+                </Row>
+              </Box>
             </Stack>
+            
             <Stack>
-              <FormControl.Label>Password</FormControl.Label>
-              <Input type="password" variant="underlined" p={2} placeholder="Password" />
+              <Box borderRadius={"md"} display={"flex"} alignSelf={"center"} shadow={2} bg={"white"} width={"75%"}>
+                <Row display={"flex"}>
+                  <Column w={"8%"} p={2}><FaLock /></Column>
+                  <Column w={"92%"}><Input variant="unstyled" w="100%" borderStyle={"none"} type="password" fontFamily={'fuenteRegular'} p={2} placeholder="Contraseña" />  </Column>
+                </Row>
+              </Box>
+            </Stack>
+            <Stack >
+              <Box w={"75%"} display={"flex"} alignSelf={"center"}>
+                <Button bg={"#008d0f"} borderRadius={"md"} borderColor={"white"} borderWidth={3}>
+                  <Row display={"flex"}>
+                    <Column><Text color={"white"} fontFamily={'fuenteRegular'} fontSize={"2xl"}>Iniciar sesión</Text></Column>
+                    <Column p={2}><FaArrowRight color={"white"} size={"17"}/></Column>
+                  </Row>
+                </Button>
+              </Box>
             </Stack>
           </Stack>
         </FormControl>
