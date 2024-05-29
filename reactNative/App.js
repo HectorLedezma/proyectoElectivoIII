8f7b11b312c55897bb8e639dfcc3f67e314b5772
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2024-05-29 14:18:56 +0000

    modificacion de sistema de estilos

--- reactNative/App.js
+++ reactNative/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainPage from './Components/main';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MainPage/>
-    </View>
+    <>
+      <View>
+        <MainPage/>
+      </View>
+    </>
   );
 }
 
--- reactNative/Components/main.jsx
+++ reactNative/Components/main.jsx
@@ -1,37 +1,30 @@
-import { View, Text,ImageBackground, StyleSheet} from "react-native";
-import fondo from '../images/fondo.png';
+
+
+
 const MainPage = () => {
     return(
-        <View style={styles.container}>
-            <ImageBackground 
-              source={fondo} 
-              resizeMode="cover" 
-              style={styles.image}
-            >
-              <Text>Hola</Text>
-            </ImageBackground>
-        </View>
+      <NativeBaseProvider>
+        <Box>Hello world</Box>
+      </NativeBaseProvider>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    image: {
-      flex: 1,
-      justifyContent: 'center',
-      height:'100vh',
-      width:'100vw'
-    },
-    text: {
-      color: 'white',
-      fontSize: 42,
-      lineHeight: 84,
-      fontWeight: 'bold',
-      textAlign: 'center',
-      backgroundColor: '#000000c0',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor:'#C0C0C0'
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
 
 export default MainPage
