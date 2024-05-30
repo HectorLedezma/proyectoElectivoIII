b2bc649c6f929053e89e6991caa0d154a03ec7cc
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-05-30 18:43:11 -0400

    adicion de pagina registro a componente de navegacion

--- reactNative/routes/Navi.jsx
+++ reactNative/routes/Navi.jsx
@@ -5,13 +5,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 import Login from "../pages/login";
 import Test from '../pages/testview';
+import Register from '../pages/register';
 
 function Navi(){
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="Register">
                 <Stack.Screen name="Test" options={{ headerShown: false }}  component={Test} />
                 <Stack.Screen name="Login" options={{ headerShown: false }} component={Login} />
+                <Stack.Screen name="Register" options={{ headerShown: false }} component={Register} />
             </Stack.Navigator>
         </NavigationContainer>
     )
