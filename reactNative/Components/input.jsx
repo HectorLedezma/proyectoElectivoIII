ff0965a9430bb476bd1bd39dcdb9d176fe60e496
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-05-30 19:55:33 -0400

    caracteristicas de un input de tipo password

--- reactNative/Components/input.jsx
+++ reactNative/Components/input.jsx
@@ -1,13 +1,43 @@
 import { Box, Column, Input, Row, Stack } from "native-base"
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 
 function Entrada(props){
+
+    const [eye,setEye] = useState(true)
+    const show = (eye) =>{
+        if(props.type === "password"){
+            return eye?"password":"text";
+        }else{
+            return "text";
+        }
+    }
+    const [text,setText] = useState(show(eye))
+
+    
+
+    const tipo = {
+        'password':<Column role="button" onTouchEnd={() =>{
+            setEye(!eye);
+            setText(show(!eye))
+        } 
+        } w={"8%"} p={2}>{eye?(<FaEye />):(<FaEyeSlash />)}</Column>,
+        'text':<></>
+    }
+
+    const col = {
+        'password':'82%',
+        'text':'92%'
+    }
+
     return(
     <Stack>
         <Box borderRadius={"md"} display={"flex"} alignSelf={"center"} shadow={2} bg={"white"} width={"75%"}>
         <Row display={"flex"}>
             <Column w={"8%"} p={2}>{props.icon}</Column>
-            <Column w={"92%"}><Input variant="unstyled" w="100%" borderStyle={"none"} type={props.type} fontFamily={'fuenteRegular'} p={2} placeholder={props.placeholder} />  </Column>
+            <Column w={col[props.type]}><Input variant="unstyled" w="100%" borderStyle={"none"} type={text} fontFamily={'fuenteRegular'} p={2} placeholder={props.placeholder} />  </Column>
+            {tipo[props.type]}
         </Row>
         </Box>
     </Stack>)
