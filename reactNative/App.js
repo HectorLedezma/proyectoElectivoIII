8593f02dabae819f8e3d9433c8c588c65a4179b0
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-05-30 17:34:15 -0400

    incorporacion de login

--- reactNative/App.js
+++ reactNative/App.js
@@ -1,11 +1,12 @@
 //import { StatusBar } from 'expo-status-bar';
 //import { StyleSheet, Text, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
-import MainPage from './Components/main';
+//import MainPage from './Components/main';
 
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { useState } from 'react';
+import Login from './pages/login';
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -37,7 +38,7 @@ export default function App() {
 
   return (
     <NativeBaseProvider config={config}>
-      <MainPage/>
+      <Login/>
     </NativeBaseProvider>
   );
 }
