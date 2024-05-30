aab41b9284b7179ba50390b7574af28a4288b229
Kensel Mckillop <kensel.mckillop@gmail.com>
2024-05-30 19:36:18 -0400

    JWT_SECRET .env

--- Backend/src/jwt.strategy.ts
+++ Backend/src/jwt.strategy.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -8,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'your_jwt_secret_key', // Cambia esto por una clave secreta segura
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
