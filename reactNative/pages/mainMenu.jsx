4692e42b8239df2ce6d5c0617b4f87e8867ae2ec
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-06-17 20:44:42 -0400

    pagina de menu principal

---
+++ reactNative/pages/mainMenu.jsx
@@ -0,0 +1,66 @@
+import { Box, Image, Text } from "native-base"
+import { useEffect, useState } from "react"
+import fondo from "../styles/fondo.json"
+import logo from "../images/Logo.png"
+import Boton from "../Components/boton"
+
+
+const MainMenu = () =>{
+    const [user,setUser] = useState({
+        "nombre":"",
+        "correo":"",
+        "otrosDatos":[]
+    })
+    useEffect(()=>{
+        //capturar datos de la BD
+        const newUser = {
+            "nombre":"Elver Galarga",
+            "correo":"aquiles.bailoyo@gmail.com",
+            "otrosDatos":[]
+        }
+        setUser(newUser);
+    });
+    return(
+        <Box style={fondo.degradado}>
+            <Box>
+                <Box alignSelf={"center"}>
+                    <Image size={"xl"} resizeMode={"contain"} source={logo} alt="logo"/>
+                </Box>
+                <Box m={5}>
+                    <Text 
+                        fontWeight={"bold"} 
+                        fontSize={"md"} 
+                        color={"white"}
+                        fontFamily={'fuenteAncha'}
+                        textAlign={"center"}
+                    >{"Bienvenido"}</Text>
+                    <Text 
+                        fontWeight={"bold"} 
+                        fontSize={"3xl"} 
+                        color={"white"}
+                        fontFamily={'fuenteAncha'}
+                        textAlign={"center"}
+                    >{user.nombre}</Text>
+                    <Text 
+                        fontWeight={"bold"}
+                        fontSize={"xl"} 
+                        color={"white"}
+                        fontFamily={'fuenteRegular'}
+                        textAlign={"center"}
+                    >{user.correo}</Text>
+                </Box>
+                <Box mt={10} mb={5} m={5}>
+                    <Boton fontSize="md" dir="izq" tipo="cuidado" title="Crear reporte" />
+                </Box>
+                <Box mt={5} m={5}>
+                    <Boton fontSize="md" dir="izq" tipo="normal" title="Ver reportes" />
+                </Box>
+            </Box>
+            <Box mt={20}>
+                <Boton fontSize="md" tipo="cuidado" title="Configuración" />
+            </Box>
+        </Box>
+    )
+}
+
+export default MainMenu
