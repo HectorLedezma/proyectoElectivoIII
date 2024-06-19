82ff42be9fdaca9f9af947c7e42f8ae8213ad17c
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-06-18 21:52:12 -0400

    pagina de menu principal en el enrutador

--- reactNative/routes/Navi.jsx
+++ reactNative/routes/Navi.jsx
@@ -6,15 +6,16 @@ const Stack = createNativeStackNavigator();
 import Login from "../pages/login";
 import Test from '../pages/testview';
 import Register from '../pages/register';
-import { Box } from 'native-base';
+import MainMenu from '../pages/mainMenu';
 
 function Navi(){
     return(
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="Main">
                 <Stack.Screen name="Test" options={{ headerShown: false }}  component={Test} />
                 <Stack.Screen name="Login" options={{ headerShown: false }} component={Login} />
                 <Stack.Screen name="Register" options={{ headerShown: false }} component={Register} />
+                <Stack.Screen name="Main" options={{ headerShown: false }} component={MainMenu} />
             </Stack.Navigator>
         </NavigationContainer>
     )
